f1e16248f1d13f4b0f571ccd15bf64238ba8887d
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-07-21 13:03:39 +0200

    Initial release. Version 0.0.1

---
+++ build.fsx
@@ -0,0 +1,45 @@
+#r @"packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+open Fake.Git
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
+open System
+open System.IO
+
+let project = "Fractal"
+let fsProjs =  !! "src/**/*.fsproj"
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let release = LoadReleaseNotes "RELEASE_NOTES.md"
+
+let genFSAssemblyInfo (projectPath) =
+    let projectName = Path.GetFileNameWithoutExtension(projectPath)
+    let basePath = "src/" + projectName
+    let fileName = basePath + "/AssemblyInfo.fs"
+    CreateFSharpAssemblyInfo fileName
+      [ Attribute.Title (projectName)
+        Attribute.Product project
+        Attribute.Version release.AssemblyVersion
+        Attribute.FileVersion release.AssemblyVersion ]
+
+Target "AssemblyInfo" (fun _ ->
+  fsProjs |> Seq.iter genFSAssemblyInfo
+)
+
+Target "Build" (fun _ ->
+    fsProjs
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
+)
+
+Target "Publish" (fun _ ->
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "%s. Version %s" release.Notes.Head release.AssemblyVersion )
+)
+
+"AssemblyInfo"
+  ==> "Build"
+  ==> "Publish"
+
+RunTargetOrDefault "Build"
---
+++ src/Fractal/AssemblyInfo.fs
@@ -0,0 +1,11 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Fractal")>]
+[<assembly: AssemblyProductAttribute("Fractal")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
